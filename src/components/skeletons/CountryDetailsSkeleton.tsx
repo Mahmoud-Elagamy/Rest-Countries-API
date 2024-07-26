d48b0a27c9afa5e43c94770e54e3756a309639a7
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-07-26 19:01:57 +0300

    made the skeleton of detailed country reponsice a little

--- src/components/skeletons/CountryDetailsSkeleton.tsx
+++ src/components/skeletons/CountryDetailsSkeleton.tsx
@@ -9,14 +9,15 @@ const CountryDetailsSkeleton = ({ isDarkMode }: CountrySkeletonProps) => {
   const highlightColor = isDarkMode ? "hsl(209, 23%, 22%)" : "#f0f0f0";
 
   return (
-    <div className="rounded border border-slate-200 p-4">
+    <div className="flex flex-wrap items-center justify-center rounded border border-slate-200 p-4 lg:justify-between">
       <Skeleton
-        className="block w-full max-w-[300px]"
-        height={200}
+        className="block w-[240px] text-center"
+        height={250}
+        width={240}
         baseColor={baseColor}
         highlightColor={highlightColor}
       />
-      <div className="space-y-6">
+      <div className="flex flex-col items-center gap-6 md:items-start">
         <Skeleton
           height={30}
           width={200}
@@ -24,17 +25,26 @@ const CountryDetailsSkeleton = ({ isDarkMode }: CountrySkeletonProps) => {
           baseColor={baseColor}
           highlightColor={highlightColor}
         />
+        <div className="flex flex-wrap justify-center gap-10">
+          <Skeleton
+            count={3}
+            width={innerWidth > 768 ? 250 : 220}
+            height={20}
+            style={{ marginTop: "0.5rem" }}
+            baseColor={baseColor}
+            highlightColor={highlightColor}
+          />
+          <Skeleton
+            count={3}
+            width={innerWidth > 768 ? 250 : 220}
+            height={20}
+            style={{ marginTop: "0.5rem" }}
+            baseColor={baseColor}
+            highlightColor={highlightColor}
+          />
+        </div>
         <Skeleton
-          count={3}
-          width={250}
-          height={20}
-          style={{ marginTop: "0.5rem" }}
-          baseColor={baseColor}
-          highlightColor={highlightColor}
-        />
-        <Skeleton
-          count={3}
-          width={250}
+          width={innerWidth > 768 ? 300 : 235}
           height={20}
           style={{ marginTop: "0.5rem" }}
           baseColor={baseColor}
