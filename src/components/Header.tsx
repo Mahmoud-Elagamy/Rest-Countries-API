a3710b212854e18af2c7984c48a51c283aecfb73
Mahmoud Elagamy <mahmoudelagamy474@gmail.com>
2024-09-19 12:56:29 +0300

    changed html anchor tag with react router link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 // Lucide Library
 import { Moon, Sun } from "lucide-react";
 
@@ -17,11 +19,11 @@ const Header = ({ isDarkMode, setIsDarkMode }: HeaderProps) => {
   return (
     <header className="mb-10 bg-white py-4 shadow-sm transition-[background-color] duration-300 ease-in-out dark:bg-dark-blue-500">
       <div className="flex-center-between container">
-        <a href="/">
+        <Link to="/">
           <h1 className="font-extrabold md:text-2xl">Where in the world?</h1>
-        </a>
+        </Link>
         <button
-          className="group/item flex items-center gap-1 rounded-md bg-slate-200 p-2 font-semibold shadow-sm transition-[background-color] duration-300 ease-in-out dark:bg-dark-blue-600"
+          className="group/item rounded-md bg-slate-200 p-2 shadow transition-[background-color] duration-300 ease-in-out dark:bg-dark-blue-600"
           type="button"
           title="Toggle Theme"
           aria-label={`Switch to ${isDarkMode ? "light" : "dark"} mode`}
@@ -29,12 +31,12 @@ const Header = ({ isDarkMode, setIsDarkMode }: HeaderProps) => {
         >
           {isDarkMode ? (
             <Sun
-              size={16}
+              size={18}
               className="duration-300 ease-in-out group-hover/item:rotate-[360deg]"
             />
           ) : (
             <Moon
-              size={16}
+              size={18}
               className="duration-300 ease-in-out group-hover/item:rotate-[360deg]"
             />
           )}
